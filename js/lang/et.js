8548f067d3ef745c1c6461adbf2931bbd21e31af
coderui <coderui@qq.com>
2018-04-22 22:30:16 +0800

    jscal2

---
+++ js/lang/et.js
@@ -0,0 +1,63 @@
+// Tõlkis: Märt Tibar, http://mardu.nohik.net
+
+Calendar.LANG("et", "Eesti keel", {
+
+        fdow: 1,                // first day of week for this locale; 0 = Sunday, 1 = Monday, etc.
+
+        goToday: "Liigu tänasele",
+
+        today: "Täna",         // appears in bottom bar
+
+        wk: "ndl",
+
+        weekend: "0,6",         // 0 = Sunday, 1 = Monday, etc.
+
+        AM: "EL",
+
+        PM: "PL",
+
+        mn : [ "jaanuar",
+               "veebruar",
+               "märts",
+               "aprill",
+               "mai",
+               "juuni",
+               "juuli",
+               "august",
+               "september",
+               "oktoober",
+               "november",
+               "detsember" ],
+
+        smn : [ "jaan",
+                "veebr",
+                "märts",
+                "apr",
+                "mai",
+                "juuni",
+                "juuli",
+                "aug",
+                "sept",
+                "okt",
+                "nov",
+                "dets" ],
+
+        dn : [ "pühapäev",
+               "esmaspäev",
+               "teisipäev",
+               "kolmapäev",
+               "neljapäev",
+               "reede",
+               "laupäev",
+               "pühapäev" ],
+
+        sdn : [ "P",
+                "E",
+                "T",
+                "K",
+                "N",
+                "R",
+                "L",
+                "P" ]
+
+});
